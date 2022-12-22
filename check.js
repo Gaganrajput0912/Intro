7df29b8ad00bb50f288d0a31e7cdcd59a9da7668
Gaganrajput0912 <101188118+Gaganrajput0912@users.noreply.github.com>
2022-12-22 15:42:49 +0530

    Add files via upload

---
+++ check.js
@@ -0,0 +1,21 @@
+namee = 'Yash Prasad'
+
+age = 25
+
+address = "Prestige Falcon City, Bangalore"
+
+console.log("personal details = My name is ", namee, "age is", age, " address is", address)
+
+console.log("personal details = My name is " + namee + "age is" + age + " address is" + address)
+
+// console.log(${personal details = My name is ${namee} age is ${age} address is ${address});
+
+// console.log(`Server is running on http://${hostname}:${port}`);
+
+var str = "My name is Yash Prasad, age is 25, address is Prestige Falcon City, Bangalore"
+
+console.log(str.split(","))
+console.log(str.split(" "))
+console.log(str.join(","))
+
+console.log()
